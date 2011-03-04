45eed1b2059d9af380098c5cdf7be8da8fdbddb6
Jason Austin <jfaustin@gmail.com>
2011-03-04 10:39:56 -0500

    Refactored duplicated code.  Modified coding style to be consistent with style standards.  Added todos for documentation purposes.

--- ncstatebrandingbar.js
+++ ncstatebrandingbar.js
@@ -1,16 +1,11 @@
 $(document).ready(function() {
-  if (!$('#ncstate-branding-bar-module').length) {
-    return;
-  }
+    if (!$('#ncstate-branding-bar-module').length) {
+        return;
+    }
 
-  /*
-   * show the nc state branding bar after the tag (which is configured on the module settings page) 
-   */
-  
-  jQuery("document").ready(function() {
-	  jQuery("#ncstate-branding-bar-module").show();
-	  $(ncstatebrandingbar_append_to_tag).filter(':first').prepend(jQuery("#ncstate-branding-bar-module"));
-  });
-  
-  
+    // show the nc state branding bar after the tag (which is configured on the module settings page)
+    jQuery("document").ready(function() {
+        jQuery("#ncstate-branding-bar-module").show();
+        $(ncstatebrandingbar_append_to_tag).filter(':first').prepend(jQuery("#ncstate-branding-bar-module"));
+    });
 });
