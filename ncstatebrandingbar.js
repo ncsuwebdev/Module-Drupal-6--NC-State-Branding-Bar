9e72d82256e0031b12afa8dc3e7f748436f762ad
Nick Young <usaussie@gmail.com>
2012-01-26 14:09:01 -0500

    fixed javascript problem that prevented bar from displaying on AFS sites

--- ncstatebrandingbar.js
+++ ncstatebrandingbar.js
@@ -1,11 +1,13 @@
-$(document).ready(function() {
-	if (!$('#ncstate-branding-bar-module').length) {
-        return;
-    }
+Drupal.behaviors.ncstatebrandingbar = function(context) {
 
-    // show the nc state branding bar after the tag (which is configured on the module settings page)
-    jQuery("document").ready(function() {
-        jQuery("#ncstate-branding-bar-module").show();
-        $(ncstatebrandingbar_append_to_tag).filter(':first').prepend(jQuery("#ncstate-branding-bar-module"));
-    });
-});
+	jQuery(document).ready(function() {
+		
+		if (!$('#ncstate-branding-bar-module').length) {
+			return;
+		}
+	
+		// show the nc state branding bar after the tag (which is configured on the module settings page)
+		jQuery("#ncstate-branding-bar-module").show();        $(ncstatebrandingbar_append_to_tag).filter(':first').prepend(jQuery("#ncstate-branding-bar-module"));
+	});
+
+};
